0759692142378fe941236cc83519ce7ef2b5507a
Joseba <josebalega@gmail.com>
2013-09-02 22:58:31 +0200

    Simplificación de uso
    Creación de carpeta automática, solo hay que personalizar el valor dos variables. El dominio del Moodle en cuestión y la cantidad de imágenes que queremos descargar.

--- moodle.py
+++ moodle.py
@@ -1,25 +1,28 @@
 import urllib2
 import os
 
+DOMINIO = 'moodle4.ehu.es'
+CANTIDAD = 1000
+
 def moodle(url, name):
-    try:
-        source = urllib2.urlopen(url).read()
-    except BaseException, e:
-        print "[!] Error:", e
-    try:
-        f = file("descargas/" + name, "wb")
-        f.write(source)
-        f.close()
-    except:
-        print "Hubo un problema con el nombre de archivo!"
-    if os.path.getsize("descargas/" + name) > 10000:
-        os.remove("descargas/" + name)
-    else:
-        print name + "=> Descargado! =>", os.path.getsize("descargas/" + name), " bytes"
+	try:
+		source = urllib2.urlopen(url).read()
+	except BaseException, e:
+		print "[!] Error:", e
+	try:
+		carpeta = "img/"
+		if not os.path.isdir(carpeta):
+			os.mkdir(carpeta)
+		f = file(carpeta + name, "wb")
+		f.write(source)
+		f.close()
+	except:
+		print "Hubo un problema con el nombre del archivo!"
+	if os.path.getsize(carpeta + name) > 10000:
+		os.remove(carpeta + name)
+	else:
+		print name + "=> Descargado! =>", os.path.getsize("descargas/" + name), "Bytes"
 
-i = 0
-
-while i < 50000:
-    name = str(i) + ".jpg"
-    moodle("http://moodle4.ehu.es/user/pix.php/" + str(i) + "/f1.jpg", name)
-    i = i + 1
+for i in range(CANTIDAD):
+	name = str(i) + ".jpg"
+	moodle('http://' + DOMINIO + '/user/pix.php/' + str(i) + '/f1.jpg', name)
