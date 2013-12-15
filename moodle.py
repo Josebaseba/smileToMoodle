89ebb3f654580a0fbbefd749e67493af0ee6ed82
Joseba <joseba@tapquo.com>
2013-12-15 19:42:56 +0100

    Mejor rendimiento

--- moodle.py
+++ moodle.py
@@ -1,28 +1,29 @@
 import urllib2
 import os
 
-DOMINIO = 'moodle4.ehu.es'
+DOMINIO = 'http://moodle4.ehu.es/'
 CANTIDAD = 1000
 
 def moodle(url, name):
-	try:
-		source = urllib2.urlopen(url).read()
-	except BaseException, e:
-		print "[!] Error:", e
-	try:
-		carpeta = "img/"
-		if not os.path.isdir(carpeta):
-			os.mkdir(carpeta)
-		f = file(carpeta + name, "wb")
-		f.write(source)
-		f.close()
-	except:
-		print "Hubo un problema con el nombre del archivo!"
-	if os.path.getsize(carpeta + name) > 10000:
-		os.remove(carpeta + name)
-	else:
-		print name + "=> Descargado! =>", os.path.getsize("descargas/" + name), "Bytes"
+  try:
+    source = urllib2.urlopen(url).read()
+  except BaseException, e:
+    print "[!] Error:", e
+  try:
+    f = file(carpeta + name, "wb")
+    f.write(source)
+    f.close()
+  except:
+    print "Hubo un problema con el nombre del archivo!"
+  if os.path.getsize(carpeta + name) > 10000:
+    os.remove(carpeta + name)
+  else:
+    print name + "=> Descargado! =>", os.path.getsize("img/" + name), "Bytes"
+
+carpeta = "img/"
+if not os.path.isdir(carpeta):
+  os.mkdir(carpeta)
 
 for i in range(CANTIDAD):
-	name = str(i) + ".jpg"
-	moodle('http://' + DOMINIO + '/user/pix.php/' + str(i) + '/f1.jpg', name)
+  name = str(i) + ".jpg"
+  moodle('http://' + DOMINIO + '/user/pix.php/' + str(i) + '/f1.jpg', name)
